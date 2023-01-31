95110baf044c7a36e79f515b574ce652a41a1a51
Kim Keiser <keiserkim69@gmail.com>
2023-01-31 09:15:01 +0100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Kim Keiser on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
